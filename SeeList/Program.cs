4ab9084456384255c7e3cf807955f6238f62af85
kile3r <kristijan.gasljevic@gmai.com>
2016-03-07 00:27:21 +0100

    Uf, jel ovo drugi?

--- SeeList/Program.cs
+++ SeeList/Program.cs
@@ -10,6 +10,39 @@ namespace SeeList
     {
         static void Main(string[] args)
         {
+            List<int> lista = new List<int>();
+
+            string more = "Y";
+
+            while (more=="Y")
+            {
+                Console.WriteLine("Unesite ne znam, neki broj: ");
+
+                lista.Add(int.Parse(Console.ReadLine()));
+                Console.WriteLine("Jos unosa?");
+                more = Console.ReadLine();
+                if (more == "Y")
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+                                
+            }//while end
+
+            Console.WriteLine("Unjeli ste:");
+
+            foreach (int a in lista)    
+            {
+                Console.WriteLine(a);
+            }
+
+            Console.ReadKey();
+
+
+
         }
     }
 }
